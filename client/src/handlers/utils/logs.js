9d9c0bdf85707b5710b37c7ac10d1b9f00531db5
Vinícius Santiago <vinicius.silva@combateafraude.com>
2020-09-20 12:00:53 -0300

    feat(logs-colors): added chalk to logs

--- client/src/handlers/utils/logs.js
+++ client/src/handlers/utils/logs.js
@@ -1,7 +1,19 @@
+const chalk = require('chalk');
+
 module.exports = (socket) => {
   socket.on('logs', (data) => {
     console.log('-------------------');
-    console.log(data.message);
+    switch (data.type) {
+      case 'success':
+        console.log(chalk.bgGreen.black(data.message));
+        break;
+      case 'error':
+        console.log(chalk.bgRed.black(data.message));
+        break;
+      default:
+        console.log(data.message);
+        break;
+    }
     console.log('-------------------');
   });
 };
